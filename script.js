0f2717bcf7fd9e634bcbcbd26162244279fb7176
Raghul S <raghul.workfolio@gmail.com>
2024-08-10 01:22:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop - 50, // Adjust for the height of the fixed header
+            behavior: 'smooth'
+        });
+    });
+});
